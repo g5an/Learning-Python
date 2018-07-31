5df4d71bc7842b58dc143cf72eac9cc6eda61dfb
lieman <15625000637@163.com>
2018-07-31 18:49:55 +0800

    explanation

--- 1.py
+++ 1.py
@@ -1,6 +1,8 @@
 #encoding:utf-8
+# python中""跟''的区别
 str1 = 'i\'m a student'  
 str2 = "i'm a student"
 str3 = "you can call me \"lieman\" "
 str4 = 'you can call me "lieman"'
-print('%s\n%s\n%s\n%s\n'%(str1,str2,str3,str4))
+print('%s\n%s\n%s\n%s\n'%(str1,str2,str3,str4)) 
+print(str1,'\n',str2,'\n',str3,'\n',str4,'\n')
