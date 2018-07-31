75b071482f1d753a5605def60d012ed8cccf7e7a
lieman <15625000637@163.com>
2018-07-31 15:40:51 +0800

    1

---
+++ 1.py
@@ -0,0 +1,6 @@
+#encoding:utf-8
+str1 = 'i\'m a student'  
+str2 = "i'm a student"
+str3 = "you can call me \"lieman\" "
+str4 = 'you can call me "lieman"'
+print('%s\n%s\n%s\n%s\n'%(str1,str2,str3,str4))
